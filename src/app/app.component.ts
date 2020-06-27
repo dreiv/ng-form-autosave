75e87f0d0ce97fe84014b1165e202cdca82b5c9f
Andrei Voicu <dreivoicu@gmail.com>
2020-06-27 10:56:16 +0300

    add persistence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil, auditTime, tap } from 'rxjs/operators';
+
+import { PersistanceService } from './persistance.service';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +13,34 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class AppComponent implements OnInit, OnDestroy {
   form: FormGroup;
 
-  constructor() {
+  private unsubscribe = new Subject<void>();
+
+  constructor(private persister: PersistanceService) {
+    const formState = this.persister.get('formState');
+
     this.form = new FormGroup({
-      name: new FormControl()
+      name: new FormControl(formState.name),
+      email: new FormControl(formState.email),
+      phone: new FormControl(formState.phone),
+      message: new FormControl(formState.message)
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form.valueChanges
+      .pipe(
+        auditTime(1000),
+        tap((formValue) => this.persister.set('formState', formValue)),
+        takeUntil(this.unsubscribe)
+      )
+      .subscribe(() => console.log('Saved'));
+  }
   ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
+    this.unsubscribe.next();
   }
 
-  onSubmit() {}
+  onSubmit(): void {
+    alert('Form Submitted!');
+    this.form.reset();
+  }
 }
