eee72cff22ac2000796bb0d448fe78680b74b616
Andrei Voicu <dreivoicu@gmail.com>
2020-06-27 10:01:27 +0300

    base structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      name: new FormControl()
+    });
+  }
+
+  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onSubmit() {}
+}
